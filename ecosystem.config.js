570c64adc8d0ef7c28bfd53c4dcc5c6637427bd6
shubham <shubham@sharma.fr>
2020-06-10 16:31:48 +0200

    every

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    script: 'www/bin',
+    watch: '.'
+  }],
+  deploy : {
+    production : {
+      user : 'shubham',
+      host : 'sharma.fr',
+      ref  : 'origin/master',
+      repo : 'git@github.com:shubs/pm2DeployDemo.git',
+      path : '/home/shubham/pm2Stuff',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
